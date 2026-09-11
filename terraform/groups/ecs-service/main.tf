43a1d0ef6a7a51240adcc018c34dbacd9d09abe9
garethpowell-ch <gpowell1@companieshouse.gov.uk>
2026-09-11 16:15:57 +0100

    ASM-2824 Update ECS service, secrets module, and dependency versions in pom.xml
    - Tomcat version updated to mitigate critical CVEs

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.413"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.423"
 
   # Environmental configuration
   environment             = var.environment
@@ -74,7 +74,7 @@ module "ecs-service" {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/parameter-store?ref=1.0.413"
+  source = "git@github.com:companieshouse/terraform-modules//aws/parameter-store?ref=1.0.423"
 
   name_prefix = "${local.service_name}-${var.environment}"
   kms_key_id  = data.aws_kms_key.kms_key.id
